d88c92b608a27bc81d254b694b8b50aac40dd795
xNoerPlaysCodes <carishma.akhauri81@gmail.com>
2026-03-30 15:38:43 +0530

    ffi: add ffi and util header

---
+++ rake_parser/ffi/util.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum {
+    RESULT_OK = 0,
+    RESULT_ERR = 1
+};
+
+union ResultData {
+    void *value;
+    const char *msg;
+};
+
+struct Result {
+    uint8_t code;
+    ResultData data;
+};
+
+#ifdef __cplusplus
+}
+#endif
+
